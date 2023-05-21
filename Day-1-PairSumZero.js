7a33f11ee6d1345dd5aa13cd3a979201a15c64bf
Surbhi Agrawal <111246726+SurbhiAgrawal9@users.noreply.github.com>
2023-05-21 13:17:38 +0530

    Update Day-1-PairSumZero.js

--- Day-1-PairSumZero.js
+++ Day-1-PairSumZero.js
@@ -1,5 +1,7 @@
 // checking pair which sum is zero
 //[p1,p2] ===0
+
+// o(n^2) quadratic time complexity
 // with for of loop
 
 ```js
@@ -22,14 +24,16 @@ console.log(result)
 
 
 //with normal for loop 
-// function getSumPair(array) {
-//   for (let i = 0; i < array.length - 1; i++) {
-//     for (let j = 1; j < array.length; j++) {
-//       if (array[i] + array[j] === 0) {
-//         return [array[i], array[j]];
-//       }
-//     }
-//   }
-// }
-// let result = getSumPair([-5, -4, -3, -2, 0, 1, 2, 4, 8, 6]);
-// console.log(result);
+```js
+function getSumPair(array) {
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 1; j < array.length; j++) {
+      if (array[i] + array[j] === 0) {
+        return [array[i], array[j]];
+      }
+    }
+  }
+}
+let result = getSumPair([-5, -4, -3, -2, 0, 1, 2, 4, 8, 6]);
+console.log(result);
+```
