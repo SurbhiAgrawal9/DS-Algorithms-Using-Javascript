7f40fc0ad0499f5b0f7ff508db4712974a6bd223
Surbhi Agrawal <111246726+SurbhiAgrawal9@users.noreply.github.com>
2023-05-25 16:46:40 +0530

    Create Day-2 Sorting.js

---
+++ Day-2 Sorting.js
@@ -0,0 +1,11 @@
+//Sorting in accending order
+
+let arr = [ -2, 46,8,22,10,-2, 4, 6,3,-4,-6,0];
+let res = arr.sort((a, b) => a - b);
+console.log(res);
+
+
+//Sorting in decending order
+
+let des = arr.sort((a, b) => b - a);
+console.log(des);
