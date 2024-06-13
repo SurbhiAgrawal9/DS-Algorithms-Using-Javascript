0328601c594f60836fda3b5ef47a5964e11768a5
Surbhi Agrawal <111246726+SurbhiAgrawal9@users.noreply.github.com>
2024-06-14 00:00:09 +0530

    Create Day-10  Two Sum.js

---
+++ Day-10  Two Sum.js
@@ -0,0 +1,34 @@
+Brute Force Approach:
+function twoSum(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+}
+
+Time Complexity: O(n²) 
+Space Complexity: O(1)
+
+
+
+  One-Pass Hash Table (Optimized):
+
+function twoSum(nums, target) {
+    const numMap = {};
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (numMap.hasOwnProperty(complement)) {
+            return [numMap[complement], i];
+        }
+        numMap[nums[i]] = i;
+    }
+}
+
+
+Time Complexity: O(n) 
+Space Complexity: O(n) 
+
+  
